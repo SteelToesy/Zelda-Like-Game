f3e8ee6ce10789e6970135fd56fb0b9c78cf1617
SteelToesy <105301671+SteelToesy@users.noreply.github.com>
2022-10-03 13:22:02 +0200

    Ready to code

---
+++ CSharp-Advanced/Assignments/Assignment1/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Assignment1
+{
+    public class GameObject
+    {
+
+        public GameObject()
+        {
+
+        }
+
+        protected Update()
+        {
+
+        }
+
+    }
+}
